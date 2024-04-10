b4648ea772d1431004b6fd1bfaab572a687bff10
nonononoka <onon.kaori@gmail.com>
2024-04-10 11:07:04 +0900

    fix

--- tutorial/pipeline_processor/ALU.v
+++ tutorial/pipeline_processor/ALU.v
@@ -21,6 +21,8 @@ module ALU(
 			aluOut = aluInA < aluInB ? `TRUE : `FALSE;
         `ALU_CODE_NOR: 
 			aluOut = !(aluInA | aluInB); 
+        default:
+            aluOut = aluInA + aluInB;
         endcase
     end
 
